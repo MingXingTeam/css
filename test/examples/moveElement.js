7daa499ab32d2d4c7ea83b0fff2f7c2a2c85a9ff
MingXingTeam <813106093@qq.com>
2014-09-09 20:53:47 +0800

    解决动画错乱

--- test/examples/moveElement.js
+++ test/examples/moveElement.js
@@ -2,6 +2,9 @@ function moveElement(elementID,final_x,final_y,interval) {
   if (!document.getElementById) return false;
   if (!document.getElementById(elementID)) return false;
   var elem = document.getElementById(elementID);
+  if(elem.movement){
+    clearTimeout(elem.movement);
+  }
   var xpos = parseInt(elem.style.left);
   var ypos = parseInt(elem.style.top);
   if (xpos == final_x && ypos == final_y) {
@@ -22,5 +25,5 @@ function moveElement(elementID,final_x,final_y,interval) {
   elem.style.left = xpos + "px";
   elem.style.top = ypos + "px";
   var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-  movement = setTimeout(repeat,interval);
+  elem.movement = setTimeout(repeat,interval);
 }
