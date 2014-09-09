83688690dd951b563b93afa1d3cc2372b36c1edc
MingXingTeam <813106093@qq.com>
2014-09-09 22:18:47 +0800

    幻灯片

--- test/scripts/global.js
+++ test/scripts/global.js
@@ -52,6 +52,46 @@ function highlightPage() {
   }
 }
 //home
+function moveElement(elementID,final_x,final_y,interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  if (elem.movement) {
+    clearTimeout(elem.movement);
+  }
+  if (!elem.style.left) {
+    elem.style.left = "0px";
+  }
+  if (!elem.style.top) {
+    elem.style.top = "0px";
+  }
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if (xpos == final_x && ypos == final_y) {
+    return true;
+  }
+  if (xpos < final_x) {
+    var dist = Math.ceil((final_x - xpos)/10);
+    xpos = xpos + dist;
+  }
+  if (xpos > final_x) {
+    var dist = Math.ceil((xpos - final_x)/10);
+    xpos = xpos - dist;
+  }
+  if (ypos < final_y) {
+    var dist = Math.ceil((final_y - ypos)/10);
+    ypos = ypos + dist;
+  }
+  if (ypos > final_y) {
+    var dist = Math.ceil((ypos - final_y)/10);
+    ypos = ypos - dist;
+  }
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  elem.movement = setTimeout(repeat,interval);
+}
+
 function prepareSlideshow() {
   if (!document.getElementsByTagName) return false;
   if (!document.getElementById) return false;
@@ -92,35 +132,8 @@ function prepareSlideshow() {
     }
   }
 }
-function moveElement(elementID,final_x,final_y,interval) {
-  if (!document.getElementById) return false;
-  if (!document.getElementById(elementID)) return false;
-  var elem = document.getElementById(elementID);
-  if(elem.movement){
-    clearTimeout(elem.movement);
-  }
-  var xpos = parseInt(elem.style.left);
-  var ypos = parseInt(elem.style.top);
-  if (xpos == final_x && ypos == final_y) {
-    return true;
-  }
-  if (xpos < final_x) {
-    xpos++;
-  }
-  if (xpos > final_x) {
-    xpos--;
-  }
-  if (ypos < final_y) {
-    ypos++;
-  }
-  if (ypos > final_y) {
-    ypos--;
-  }
-  elem.style.left = xpos + "px";
-  elem.style.top = ypos + "px";
-  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-  elem.movement = setTimeout(repeat,interval);
-}
+
+
 //加载事件
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
